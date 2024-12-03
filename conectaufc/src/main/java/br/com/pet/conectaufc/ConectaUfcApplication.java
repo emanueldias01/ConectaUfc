5d6ac1eb3048fa0217735850ab152ed11faabb9b
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-12-03 10:21:22 -0300

    upload de arquivos

---
+++ conectaufc/src/main/java/br/com/pet/conectaufc/ConectaUfcApplication.java
@@ -0,0 +1,13 @@
+package br.com.pet.conectaufc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaUfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaUfcApplication.class, args);
+	}
+
+}
---
+++ conectaufc/src/test/java/br/com/pet/conectaufc/ConectaUfcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pet.conectaufc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaUfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
