0cafc200163989ada851b8b1bf823017b8b4c5c6
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-12-26 13:46:05 -0300

    criacao do filtro do token

---
+++ conectaufc/src/main/java/br/com/pet/conectaufc/infra/security/FilterToken.java
@@ -0,0 +1,24 @@
+package br.com.pet.conectaufc.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class FilterToken extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //recupera token da requisicao
+
+        String token;
+
+        if(token != null){
+            //valida token
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- conectaufc/src/main/java/br/com/pet/conectaufc/infra/security/SecurityConfiguration.java
+++ conectaufc/src/main/java/br/com/pet/conectaufc/infra/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.pet.conectaufc.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,16 +11,21 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private FilterToken filterToken;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(csrf -> csrf.disable())
+                .addFilterBefore(filterToken, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(
                 auth -> auth.anyRequest().permitAll()
         ).build();
